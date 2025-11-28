29f84ccd4977be7f475349eac80e5e15f38aee5a
tac-aid <tacad.wrk@gmail.com>
2025-11-28 20:23:55 +0900

    キーマップ修正

--- keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
+++ keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_LSFT,    KC_NO, KC_NO, KC_NO, KC_NO
     ),
     [4] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_NO,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_NO,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_NO,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_NO,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_NO,   KC_NO,
         KC_NO, KC_NO, KC_LALT,KC_LGUI,    KC_LNG2,    KC_SPC,     KC_ENT,    KC_LNG1, KC_NO, KC_NO, KC_NO, KC_NO
