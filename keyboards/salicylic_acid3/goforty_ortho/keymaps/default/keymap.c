a8b2d54c1af884ee99b0f85d1b1ca7a177074c55
tac-aid <tacad.wrk@gmail.com>
2025-11-28 19:48:11 +0900

    キーマップ修正

--- keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
+++ keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,  KC_L,    KC_U,    KC_SCLN, LSFT(KC_SCLN), KC_NO, KC_F, KC_W, KC_R, KC_Y, KC_P, KC_BSPC,
         KC_LCTL,        KC_E,  KC_I,    KC_A,    KC_O,    KC_COMM,       KC_NO, KC_K, KC_T, KC_N, KC_S, KC_H, LGUI(KC_SPC),
         LSFT_T(KC_ESC), KC_Z,  KC_X,    KC_C,    KC_V,    KC_DOT,        KC_NO, KC_G, KC_D, KC_M, KC_J, KC_B, KC_NO,
-        KC_NO,          KC_NO, KC_LALT, KC_LGUI, USER_2,LT(3, KC_SPC),LSFT_T(KC_ENT),USER_3, KC_NO, KC_NO, LGUI(LSHFT(LALT(LCTL(KC_S)))), KC_NO
+        KC_NO,          KC_NO, KC_LALT, KC_LGUI, USER_2,LT(3, KC_SPC),LSFT_T(KC_ENT),USER_3, KC_NO, KC_NO, HYPR(KC_S), KC_NO
     ),
     [1] = LAYOUT(
         KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, LSFT(KC_SCLN), KC_1, KC_2, KC_3, KC_NO, KC_NO, 
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, KC_DOT, _______, _______, _______, _______, _______, 
+        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______,   _______,   _______, _______,   _______,    _______, _______, _______, _______
+        _______, _______, KC_LALT, KC_LGUI,   USER_2,   _______, _______,   _______,    _______, _______, _______, _______
     ),
     [3] = LAYOUT(
         KC_NO, KC_NO, KC_NO, LSFT(KC_TAB), USER_1, KC_NO, KC_NO, KC_NO, LALT(KC_LEFT), KC_UP, LALT(KC_RIGHT), KC_NO, KC_NO, 
