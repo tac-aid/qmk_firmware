f89111b1741b614255b55754067a17a315826f64
tac-aid <tacad.wrk@gmail.com>
2025-11-28 20:18:32 +0900

    キーマップ修正

--- keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
+++ keyboards/salicylic_acid3/goforty_ortho/keymaps/default/keymap.c
@@ -38,22 +38,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,   USER_3,    KC_NO, KC_NO, KC_NO, KC_NO
     ),
     [2] = LAYOUT(
-        _______, KC_ASTR, KC_SLSH, KC_GRV, KC_DLR, KC_COMM, KC_NO, KC_DOT, KC_LBRC, KC_RBRC, KC_EXLM, KC_QUES, _______, 
+        _______, KC_ASTR, KC_SLSH, KC_GRV, KC_DLR, KC_COMM, KC_NO, KC_DOT, KC_LBRC, KC_RBRC, KC_EXLM, KC_QUES, KC_NO, 
         KC_LCTL, KC_PLUS, KC_MINS, KC_DOUBLE_QUOTE, KC_EQL, KC_PERC, KC_NO, KC_AT, KC_LPRN, KC_RPRN, KC_LABK, KC_RABK, _______,
         _______, KC_CIRC, KC_TILD, KC_QUOT, KC_UNDS, KC_BSLS, KC_NO, KC_HASH, KC_LCBR, KC_RCBR, KC_AMPR, KC_PIPE, _______,
         _______, _______, KC_LALT, KC_LGUI,   USER_2,   KC_SPC, _______,   _______,    _______, _______, _______, _______
     ),
     [3] = LAYOUT(
         KC_NO, KC_NO, KC_NO, LSFT(KC_TAB), USER_1, KC_NO, KC_NO, KC_NO, LALT(KC_LEFT), KC_UP, LALT(KC_RIGHT), KC_NO, KC_NO, 
-        KC_NO, KC_NO, LALT(KC_S), LSFT(KC_TAB), USER_0, KC_NO, KC_NO, KC_PGUP, KC_LEFT, KC_DOWN, KC_RIGHT, LCTL(KC_LEFT), LCTL(KC_LEFT),
-        KC_NO, KC_NO, LALT(KC_X), LALT(KC_C), KC_NO, KC_NO, KC_NO, KC_PGDN, LGUI(KC_LEFT), KC_NO, LGUI(KC_RIGHT), LCTL(KC_LEFT), KC_F11,
+        KC_NO, KC_NO, LALT(KC_S), LSFT(KC_TAB), USER_0, KC_NO, KC_NO, KC_PGUP, KC_LEFT, KC_DOWN, KC_RIGHT, LCTL(KC_LEFT), LCTL(KC_RIGHT),
+        KC_NO, KC_NO, LALT(KC_X), LALT(KC_C), KC_NO, KC_NO, KC_NO, KC_PGDN, LGUI(KC_LEFT), KC_NO, LGUI(KC_RIGHT), LCTL(KC_UP), KC_F11,
         KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_LSFT,    KC_NO, KC_NO, KC_NO, KC_NO
     ),
     [4] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_NO,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_NO,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_NO,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_NO,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_NO,   KC_NO,
-        KC_NO, KC_NO, KC_LALT,KC_LGUI,    KC_LNG1,    KC_SPC,     KC_ENT,    KC_LNG2, KC_NO, KC_NO, KC_NO, KC_NO
+        KC_NO, KC_NO, KC_LALT,KC_LGUI,    KC_LNG2,    KC_SPC,     KC_ENT,    KC_LNG1, KC_NO, KC_NO, KC_NO, KC_NO
     ),
     [5] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
